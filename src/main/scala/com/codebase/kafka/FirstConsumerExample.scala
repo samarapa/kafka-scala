620bce5164a466f74e80180c4c676a9237a8130a
samarapa <senthilkumar_amarapathy@permierinc.com>
2017-02-27 16:49:51 -0500

    1st commit

---
+++ src/main/scala/com/codebase/kafka/FirstConsumerExample.scala
@@ -0,0 +1,31 @@
+package com.codebase.kafka
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+
+/**
+  * Created by samarapa on 2/27/2017.
+  */
+object FirstConsumerExample {
+  def main(args: Array[String]) {
+    val brokers = args(2);
+    val props = new Properties();
+    props.put("bootstrap.servers", brokers);
+    props.put("client.id", "ProducerExample");
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+    val producer = new KafkaProducer[String,String](props);
+    val topic = "test";
+    for (i <- 1 to 50){
+      val record = new ProducerRecord(topic, "key",s"hello $i")
+      producer.send(record);
+    }
+    val record = new ProducerRecord(topic, "key",s"End of message")
+    producer.send(record);
+    producer.close();
+  }
+
+}
