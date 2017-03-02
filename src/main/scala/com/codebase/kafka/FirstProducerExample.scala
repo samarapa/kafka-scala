c3f026496750e6c020e5a940362ada8c834b20f1
samarapa <senthilkumar_amarapathy@permierinc.com>
2017-03-01 21:56:50 -0500

    final commit

--- src/main/scala/com/codebase/kafka/FirstProducerExample.scala
+++ src/main/scala/com/codebase/kafka/FirstProducerExample.scala
@@ -10,22 +10,30 @@ import org.apache.kafka.clients.producer.ProducerRecord
   */
 object FirstProducerExample {
   def main(args: Array[String]) {
-    val brokers = args(2);
+    val brokers = "192.168.0.31:9092"
     val props = new Properties();
     props.put("bootstrap.servers", brokers);
     props.put("client.id", "ProducerExample");
     props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-    val producer = new KafkaProducer[String,String](props);
+    println("Kafka Produce Start")
+    val producer = new KafkaProducer[String, String](props);
+    //println("Kafka Produce ended")
     val topic = "test";
-    for (i <- 1 to 50){
-      val record = new ProducerRecord(topic, "key",s"hello $i")
+    try {
+      println("Start sending messages...!")
+      for (i <- 1 to 5) {
+        val record = new ProducerRecord(topic, "key", s"hello $i")
+         producer.send(record);
+            }
+      val record = new ProducerRecord(topic, "key", s"End of message")
       producer.send(record);
+      println("Finish sending messages...!")
+      producer.close();
+      println("Producer connection Closed...!")
+       } catch{
+      case e: Exception => { e.printStackTrace(); e.toString() }
     }
-    val record = new ProducerRecord(topic, "key",s"End of message")
-    producer.send(record);
-    producer.close();
   }
 
 }
